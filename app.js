e8232a541d1a5807f44fa01c4819c604844e8ce4
Muwaffaq <itzmuwaffaq@gmail.com>
2021-08-20 18:37:25 +0100

    added comma

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/contact', function(req, res){
 })
 app.post('/contact', urlencodedParser, function(req, res){
    res.render('contact-success', { data : req.body})
-})
+}),
 
 app.get('/profile/:name', function(req, res){
    var data = {age : 29, job : 'ninja', hobbies : ['eating', 'fishing', 'dancing', 'running']}
