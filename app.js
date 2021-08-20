e49f1ec0b67f69b213786d58c22c6ec5e49777fb
Muwaffaq <itzmuwaffaq@gmail.com>
2021-08-20 18:34:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var path
+var bodyParser = require('body-parser')
+
+var app = express()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   res.render('index')
+})
+app.get('/contact', function(req, res){
+   res.render('contact', {qs : req.query})
+})
+app.post('/contact', urlencodedParser, function(req, res){
+   res.render('contact-success', { data : req.body})
+})
+
+app.get('/profile/:name', function(req, res){
+   var data = {age : 29, job : 'ninja', hobbies : ['eating', 'fishing', 'dancing', 'running']}
+   res.render('profile', {person : req.params.name , data : data})
+})
+
+app.listen(3000)
